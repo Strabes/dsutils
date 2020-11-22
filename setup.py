541c28ea088f1350a206069b189bc6527a6d16e6
Greg Strabel <Strabes@users.noreply.github.com>
2020-11-22 15:58:44 -0700

    start unit tests

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python Utilities for Data Science",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Strabes/dsutils",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
