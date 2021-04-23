cf93e137fd0e459897ce6b335c96f4183de18812
Strabes <Strabes@users.noreply.github.com>
2021-04-23 16:30:53 -0600

    use feature-engine base classes

--- dsutils/transformers/__init__.py
+++ dsutils/transformers/__init__.py
@@ -1,40 +1,20 @@
-from ._base import BaseTransformer
-
-from ._categorical_binners import _CategoricalBinner
+"""Init transformers"""
 from ._categorical_binners import MaxLevelBinner
 from ._categorical_binners import PercentThresholdBinner
 from ._categorical_binners import CumulativePercentThresholdBinner
 
-from ._numeric_transformers import OutlierPercentileCapper
+#from ._numeric_transformers import OutlierPercentileCapper
 
 from ._transform_wrapper import TransformWrapper
 
-# from ._missing_transformers import BaseMissingTransformer
-# from ._missing_transformers import MissingIndicator
-# from ._missing_transformers import ReplaceMissingMean
-# from ._missing_transformers import ReplaceMissingMedian
-# from ._missing_transformers import ReplaceMissingNumericConstant
-# from ._missing_transformers import ReplaceMissingCategorical
-
-from ._date_transformers import DateComponents
-
 __all__ = [
-    'BaseTransformer',
     # from _categorical_binners
-    '_CategoricalBinner',
     'MaxLevelBinner',
     'PercentThresholdBinner',
     'CumulativePercentThresholdBinner',
-    # _missing_transformers
-#     'BaseMissingTransformer',
-#     'MissingIndicator',
-#     'ReplaceMissingMean',
-#     'ReplaceMissingMedian',
-#     'ReplaceMissingNumericConstant',
-#     'ReplaceMissingCategorical',
     # _date_transformers
     'DateComponents',
     # _numeric_transformers
-    'OutlierPercentileCapper',
+    #'OutlierPercentileCapper',
     'TransformWrapper'
 ]
