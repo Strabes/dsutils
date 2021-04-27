dd9f12d51ef9d3ff45f9238bd80d84d8da365472
Strabes <Strabes@users.noreply.github.com>
2021-04-27 06:32:13 -0600

    categorical_histogram unit test

--- tests/test_histograms.py
+++ tests/test_histograms.py
@@ -1,16 +1,22 @@
+"""
+Test histogram plots
+To compile baseline images, run:
+pytest --mpl-generate-path=tests/baseline
+"""
+
 import pytest
 import pandas as pd
-from dsutils.utils.histograms import numeric_histogram
+from dsutils.utils.histograms import numeric_histogram, categorical_histogram
 
 @pytest.fixture
 def example_data():
-    return(
-        pd.DataFrame(
-           {'x':[
-               0, 1, 2.2, 1, 3.1,
-               -0.23, 1, 2.3, 0, -0.5,
-               2, 1.1 ]}
-    ))
+    """Data for test"""
+    x = [0, 1, 2.2, 1, 3.1,
+        -0.23, 1, 2.3, 0, -0.5,
+        2, 1.1 ]
+    y = list('aaaaabbbccde')
+    df = pd.DataFrame({'x':x,'y':y})
+    return df
 
 @pytest.mark.mpl_image_compare
 def test_numeric_histogram(example_data):
@@ -22,4 +28,15 @@ def test_numeric_histogram(example_data):
       stat = 'mean',
       min_levels = 5,
       normalize = True)
+    return nh
+
+
+@pytest.mark.mpl_image_compare
+def test_categorical_histogram(example_data):
+    nh = categorical_histogram(
+        example_data,
+        x='y',
+        line_columns='x',
+        max_levels=3,
+        normalize=True)
     return nh
